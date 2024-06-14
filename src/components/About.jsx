f6f5637b675c3dacb8954e388b6410e6c93d0c17
Jhan Marmel <jhanmarmel@gmail.com>
2024-06-14 14:00:08 +0800

    react intersection observer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,19 @@
 import "./styles/about.css";
 import "./styles/main-styles.css";
+import { InView } from "react-intersection-observer";
 export default function About({ about }) {
   return (
     <div className="about page" ref={about}>
-      <h2 className="page-header">About</h2>
-      <div className="about-content">
-        <div className="about-me"></div>
-        <div className="about-skills"></div>
-      </div>
+      <InView
+        as="div"
+        onChange={(inView, entry) => console.log("Inview:", inView)}
+      >
+        <h2 className="page-header">About</h2>
+        <div className="about-content">
+          <div className="about-me"></div>
+          <div className="about-skills"></div>
+        </div>
+      </InView>
     </div>
   );
 }
